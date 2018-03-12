e5e431de0a86efb70dedf35f26e335d782cbdfb7
akiraandy <andypschwartz@gmail.com>
2018-03-12 16:56:04 -0500

    make classes port, server httpresponse and empty tests for those classs

---
+++ src/test/java/HttpResponse/HttpResponseTest.java
@@ -0,0 +1,17 @@
+package HttpResponse;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpResponseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ src/test/java/Server/ServerTest.java
@@ -0,0 +1,17 @@
+package Server;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
