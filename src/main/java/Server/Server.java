c5d45b652de828ed240b480b09787d6bd722d459
akiraandy <andypschwartz@gmail.com>
2018-03-14 16:37:30 -0500

    adjust control flow for server main function

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -1,35 +1,31 @@
 package Server;
+import ClientIO.ClientInput;
+import ClientIO.ClientOutput;
+import Handlers.ResponseHandler;
+import HttpResponse.HttpResponse;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public ServerSocket socket;
-    private Socket sock;
+    private HttpResponse httpResponse;
 
     public Server(int portNum) throws IOException {
         String message = "HTTP/2.0 200 OK";
         this.socket = new ServerSocket(portNum);
-        while(true) {
-            System.out.println("Waiting for client...");
-            Socket sock = socket.accept();
-            System.out.println("Client connected");
-            InputStreamReader stream = new InputStreamReader(sock.getInputStream());
-            BufferedReader reader = new BufferedReader(stream);
-            String newMessage = reader.readLine();
-            System.out.println(newMessage);
-            PrintWriter writer = new PrintWriter(sock.getOutputStream());
-            writer.println(message);
-            writer.close();
-            System.out.println(message);
+        while (true) {
+        System.out.println("Waiting for client...");
+        Socket connectedSocket = socket.accept();
+        ClientInput clientInput = new ClientInput(connectedSocket);
+        System.out.println("Client connected");
+        String rawRequest = clientInput.getRawRequestString();
+        httpResponse = ResponseHandler.buildResponse(rawRequest);
+        System.out.println(rawRequest);
+        ClientOutput clientOutput = new ClientOutput(connectedSocket);
+        clientOutput.writeTo(httpResponse.full_response());
+        System.out.println(httpResponse.full_response());
         }
-    }
-
-    public InputStreamReader getInputStream() throws IOException {
-        return new InputStreamReader(sock.getInputStream());
     }
 }
