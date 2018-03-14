251eca6b33eead974ff9d313a31e676df32b0e5e
akiraandy <andypschwartz@gmail.com>
2018-03-14 16:32:52 -0500

    HttpRequest creates new httpRequestParser upon creation and then assigns it's own attributes based on the parser

--- src/main/java/HttpRequest/HttpRequest.java
+++ src/main/java/HttpRequest/HttpRequest.java
@@ -1,15 +1,19 @@
 package HttpRequest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.util.HashMap;
 
 public class HttpRequest implements IHttpRequest {
     private String method, version, path, requestLine;
     private HashMap<String, String> headers, params;
+    private HttpRequestParser httpRequestParser;
 
-    public HttpRequest(BufferedReader reader) throws IOException {
-//        parseRequestLine(reader.readLine());
+    public HttpRequest(String request) {
+        httpRequestParser = new HttpRequestParser(request);
+        this.method = httpRequestParser.method;
+        this.version = httpRequestParser.version;
+        this.path = httpRequestParser.path;
+        this.requestLine = httpRequestParser.requestLine;
+        this.headers = httpRequestParser.headers;
     }
 
     @Override
