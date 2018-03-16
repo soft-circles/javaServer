278ea3a61e5f580a34a255d42e2e56ede3ea713d
akiraandy <andypschwartz@gmail.com>
2018-03-16 11:55:12 -0500

    change directory structure

--- src/main/java/HttpRequest/IHttpRequest.java
+++
@@ -1,8 +0,0 @@
-package HttpRequest;
-
-public interface IHttpRequest {
-    String requestLine();
-    String method();
-    String path();
-    String version();
-}
--- src/main/java/HttpResponse/Routes.java
+++
@@ -1,7 +0,0 @@
-package HttpResponse;
-
-import java.util.List;
-
-public class Routes {
-
-}
