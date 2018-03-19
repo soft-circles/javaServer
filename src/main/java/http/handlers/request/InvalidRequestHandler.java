193bea0a025f610aeabe55e68b0561b6fb20c93d
akiraandy <andypschwartz@gmail.com>
2018-03-19 16:32:03 -0500

    create invalid requestrequesthandler

--- src/main/java/http/handlers/request/InvalidRequestHandler.java
+++ src/main/java/http/handlers/request/InvalidRequestHandler.java
@@ -1,4 +1,20 @@
 package http.handlers.request;
 
-public class InvalidRequestHandler {
+import http.request.HttpRequest;
+import http.response.HttpResponse;
+import http.status.StatusMessages;
+
+public class InvalidRequestHandler implements IRequestHandler {
+    public InvalidRequestHandler(HttpRequest httpRequest) {
+    }
+
+    @Override
+    public HttpResponse returnResponse() {
+        HttpResponse httpResponse = new HttpResponse();
+        httpResponse.setStatus("404");
+        httpResponse.addHeader("Content-Type", "text/html");
+        httpResponse.setReasonPhrase(StatusMessages.STATUSES.get(404).toString());
+        httpResponse.setBody("Content not found");
+        return httpResponse;
+    }
 }
