65a81fe5256abb157b9b9d03a1797a1549de2069
akiraandy <andypschwartz@gmail.com>
2018-03-19 16:36:45 -0500

    create hash of routes

--- src/main/java/http/router/Routes.java
+++ src/main/java/http/router/Routes.java
@@ -1,4 +1,22 @@
 package http.router;
 
+import http.method.httpMethod;
+
+import java.util.*;
+
 public class Routes {
+
+    public static final Map<String, ArrayList<httpMethod>> VALID_PATHS = paths();
+
+    private static Map<String, ArrayList<httpMethod>> paths() {
+        Map<String, ArrayList<httpMethod>> paths = new HashMap<>();
+        paths.put("/", new ArrayList<>(Arrays.asList(httpMethod.GET)));
+        paths.put("/put-target", new ArrayList<>(Arrays.asList(httpMethod.PUT)));
+        paths.put("/form", new ArrayList<>(Arrays.asList(httpMethod.POST)));
+        paths.put("/redirect", new ArrayList<>(Arrays.asList(httpMethod.GET)));
+        paths.put("/coffee", new ArrayList<>(Arrays.asList(httpMethod.GET)));
+        paths.put("/tea", new ArrayList<>(Arrays.asList(httpMethod.GET)));
+        return paths;
+    }
 }
+
