2688db2b0b8fbfd4ff9e185a33039b1f0f59da33
akiraandy <andypschwartz@gmail.com>
2018-03-19 16:35:52 -0500

    add router class and switch statement for handlers

--- src/main/java/http/router/Router.java
+++ src/main/java/http/router/Router.java
@@ -1,4 +1,32 @@
 package http.router;
 
+import http.handlers.request.*;
+import http.request.HttpRequest;
+
 public class Router {
+
+    private Router(){}
+
+    public static IRequestHandler getHandler(HttpRequest httpRequest) {
+        return selectHandler(httpRequest);
+    }
+
+    private static IRequestHandler selectHandler(HttpRequest httpRequest) {
+        IRequestHandler handler;
+        switch (httpRequest.method()) {
+            case GET: handler = new DirectoryHandler(httpRequest);
+                        break;
+            case PUT: handler = new PutRequestHandler(httpRequest);
+                        break;
+            case POST: handler = new PostRequestHandler(httpRequest);
+                        break;
+            case OPTIONS: handler = new OptionsRequestHandler(httpRequest);
+                        break;
+            case DELETE: handler = new DeleteRequestHandler(httpRequest);
+                        break;
+            default: handler = new InvalidRequestHandler(httpRequest);
+                         break;
+        }
+        return handler;
+    }
 }
