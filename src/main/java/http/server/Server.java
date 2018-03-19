8dfd6ea51034f7c7e58bf43e85a105a47b1c27e2
akiraandy <andypschwartz@gmail.com>
2018-03-19 16:37:45 -0500

    change main server loop

--- src/main/java/http/server/Server.java
+++ src/main/java/http/server/Server.java
@@ -1,8 +1,11 @@
 package http.server;
 import http.IO.ClientInput;
 import http.IO.ClientOutput;
+import http.handlers.request.IRequestHandler;
 import http.handlers.response.ResponseHandler;
+import http.request.HttpRequest;
 import http.response.HttpResponse;
+import http.router.Router;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -18,11 +21,17 @@ public class Server {
         ClientInput clientInput = new ClientInput(connectedSocket);
         System.out.println("Client connected");
         String rawRequest = clientInput.getRawRequestString();
-        HttpResponse httpResponse = ResponseHandler.buildResponse(rawRequest);
+        HttpRequest httpRequest = new HttpRequest(rawRequest);
         System.out.println(rawRequest);
+        IRequestHandler handler = Router.getHandler(httpRequest);
+        HttpResponse httpResponse = handler.returnResponse();
         ClientOutput clientOutput = new ClientOutput(connectedSocket);
         clientOutput.writeTo(httpResponse.fullResponse());
         System.out.println(httpResponse.fullResponse());
         }
     }
 }
+
+
+
+
