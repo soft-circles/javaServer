4f1a01be2bda42471c524a4869144364843e7ecc
akiraandy <andypschwartz@gmail.com>
2018-03-20 16:44:10 -0500

    implement put and post request handlers

--- src/main/java/http/handlers/request/PutRequestHandler.java
+++ src/main/java/http/handlers/request/PutRequestHandler.java
@@ -1,14 +1,33 @@
 package http.handlers.request;
 
+import http.handlers.directory.InvalidResourceHandler;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
+import http.status.StatusMessages;
+import http.utils.PathChecker;
 
 public class PutRequestHandler implements IRequestHandler {
+    private final HttpRequest httpRequest;
+    private final InvalidResourceHandler invalidResourceHandler;
+
     public PutRequestHandler(HttpRequest httpRequest) {
+        this.httpRequest = httpRequest;
+        this.invalidResourceHandler = new InvalidResourceHandler(httpRequest);
     }
 
     @Override
     public HttpResponse returnResponse() {
-        return null;
+        if (PathChecker.validRoute(httpRequest.path())) {
+            return createResponse();
+        } else  {
+            return invalidResourceHandler.generateResponse();
+        }
+    }
+
+    private HttpResponse createResponse() {
+        HttpResponse httpResponse = new HttpResponse();
+        httpResponse.setStatus("200");
+        httpResponse.setReasonPhrase(StatusMessages.STATUSES.get(200).toString());
+        return httpResponse;
     }
 }
