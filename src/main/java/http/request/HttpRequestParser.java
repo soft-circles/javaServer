43aa89ea6350d4bff17a03f67859ba58e332289e
akiraandy <andypschwartz@gmail.com>
2018-03-27 12:11:45 -0500

    add println

--- src/main/java/http/request/HttpRequestParser.java
+++ src/main/java/http/request/HttpRequestParser.java
@@ -45,6 +45,7 @@ public class HttpRequestParser {
         } catch (Exception e) {
             setMethod(httpMethod.INVALID);
         }
+        System.out.println(requestLine);
         String[] pathAndParameters = requestLine.split(" ")[1].split("\\?", 2);
         setPath(pathAndParameters[0]);
         setVersion(requestLine.split(" ")[2]);
