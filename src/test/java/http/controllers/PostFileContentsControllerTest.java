9aa91932169c25be96c6df0a695d6eac5edcee86
akiraandy <andypschwartz@gmail.com>
2018-04-05 10:57:02 -0500

    setup test for PostFileContentsController

--- src/test/java/http/controllers/PostFileContentsControllerTest.java
+++ src/test/java/http/controllers/PostFileContentsControllerTest.java
@@ -1,5 +1,7 @@
 package http.controllers;
 
+import http.IO.file.FileIO;
+import http.router.Router;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,8 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PostFileContentsControllerTest {
 
+    private PostFileContentsController postFileContentsController;
+
     @BeforeEach
     void setUp() {
+        FileIO fileIO = new FileIO("./public");
+        Router router = new Router();
+        postFileContentsController = new PostFileContentsController(router, fileIO);
     }
 
     @Test
