bc8f64ed31b9e27a5301577f6e7d1597092fb3af
akiraandy <andypschwartz@gmail.com>
2018-04-23 14:59:52 -0500

    pass router as argument to server

--- src/main/java/http/server/Server.java
+++ src/main/java/http/server/Server.java
@@ -18,12 +18,24 @@ import java.net.ServerSocket;
 import java.util.Arrays;
 
 public class Server {
+    private final String directory;
+    private final int portNumber;
     private Router router;
 
-    public Server(int portNum, String directory) throws IOException {
-        router = configServer(new FileIO(directory));
-        System.out.println("Server starting on port: " + String.valueOf(portNum) + " " + "with public directory: " + directory);
-        ServerSocket socket = new ServerSocket(portNum);
+    public Server(int portNum, String directory, Router router) throws IOException {
+        this.router = router;
+        this.portNumber = portNum;
+        this.directory = directory;
+    }
+
+    public void run() {
+        System.out.println("Server starting on port: " + String.valueOf(portNumber) + " " + "with public directory: " + directory);
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         ConnectionProcessMultiThread connectionProcessMultiThread = new ConnectionProcessMultiThread();
         while (true) {
             IClient client = getiClient(socket);
@@ -32,45 +44,9 @@ public class Server {
             connectionProcessMultiThread.execute(httpConnectionToProcess);
         }
     }
-
     private IClient getiClient(ServerSocket socket) {
         System.out.println("Waiting for client...");
         return new Client(socket);
-    }
-
-    private Router configServer(IFileIO IFileIO) {
-        Router router = new Router();
-        DirectoryController dirHandler = new DirectoryController(IFileIO);
-        PostFileContentsController postFileHandler = new PostFileContentsController(IFileIO);
-        EditFileController editFileController = new EditFileController(IFileIO);
-        LogsController logsController = new LogsController();
-        IAuth logAuthHandler = new AuthHandler("admin", "hunter2");
-        CookiesController cookiesController = new CookiesController();
-        PatchController patchController = new PatchController(IFileIO);
-        PartialContentController partialContentController = new PartialContentController(IFileIO);
-
-        router.addRoute("/", Arrays.asList(HttpMethod.GET, HttpMethod.HEAD), dirHandler);
-        router.addRoute("/coffee", HttpMethod.GET, new TeaPotController());
-        router.addRoute("/tea", HttpMethod.GET, new TeaPotController());
-        router.addRoute("/form", Arrays.asList(HttpMethod.GET, HttpMethod.POST), postFileHandler);
-        router.addRoute("/put-target", Arrays.asList(HttpMethod.GET, HttpMethod.PUT), editFileController);
-        router.addRoute("/redirect", HttpMethod.GET, new RedirectController());
-        router.addRoute("/file1", HttpMethod.GET, dirHandler);
-        router.addRoute("/file2", HttpMethod.GET, dirHandler);
-        router.addRoute("/text-file.txt", HttpMethod.GET, dirHandler);
-        router.addRoute("/image.jpeg", HttpMethod.GET, dirHandler);
-        router.addRoute("/image.gif", HttpMethod.GET, dirHandler);
-        router.addRoute("/image.png", HttpMethod.GET, dirHandler);
-        router.addRoute("/cat-form", Arrays.asList(HttpMethod.GET, HttpMethod.POST), new CatFormController(router, IFileIO));
-        router.addRoute("/method_options", Arrays.asList(HttpMethod.OPTIONS, HttpMethod.GET, HttpMethod.PUT, HttpMethod.POST, HttpMethod.HEAD), new MethodOptionsController(router));
-        router.addRoute("/method_options2", Arrays.asList(HttpMethod.GET, HttpMethod.OPTIONS, HttpMethod.HEAD), new MethodOptionsController(router));
-        router.addRoute("/parameters", HttpMethod.GET, new ParameterDecodeController());
-        router.addRouteWithAuth("/logs", HttpMethod.GET, logsController, logAuthHandler);
-        router.addRoute("/cookie", HttpMethod.GET, cookiesController);
-        router.addRoute("/eat_cookie", HttpMethod.GET, cookiesController);
-        router.addRoute("/patch-content.txt", Arrays.asList(HttpMethod.GET, HttpMethod.PATCH), patchController);
-        router.addRoute("/partial_content.txt", HttpMethod.GET, partialContentController);
-        return router;
     }
 }
 
